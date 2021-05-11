4a1d9f95b25c35236eba4a2c408e97476f630d92
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-10 23:53:00 -0300

    implementing queries

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
 package graph
 
+import "go-graphql/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Categories []*model.Category // In memory, could be a db repository, or an API...
+	Courses    []*model.Course
+	Chapters   []*model.Chapter
+}
